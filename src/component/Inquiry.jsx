7e8e5c988c6a61716eb833de38d3190512d19fd4
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-08-23 12:28:30 +0530

    ButtonFix

--- src/component/Inquiry.jsx
+++ src/component/Inquiry.jsx
@@ -53,13 +53,15 @@ const Inquiry = () => {
                   <p className="text-gray-600 text-left mb-4">
                     {descriptions[index]}
                   </p>
-                  <button className="bg-zinc-950 text-white px-4 py-2 rounded hover:bg-red-500 transition duration-300">
                   <Link onClick={handleClick} to={`/${title.toLowerCase()}`}>
+                  
+                  <button className="bg-zinc-950 text-white px-4 py-2 rounded hover:bg-red-500 transition duration-300">
                    More
-                   </Link>
                    
                     
                   </button>
+                  </Link>
+                  
                 </div>
               </div>
             ))}
