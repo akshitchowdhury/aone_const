ae343a43849727e083cd1823fe86842c2e2c6aa0
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-08-29 12:31:32 +0530

    AboutWhtWeDo4

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import h1 from "../assets/images/web1.jpg";
-import h2 from "../assets/images/web2.jpg"
-import h3 from "../assets/images/web3.jpg";
+import h1 from "../assets/images/homeBg.jpg";
+import h2 from "../assets/images/homeBg.jpg"
+import h3 from "../assets/images/homeBg.jpg";
 import Hero2 from './Hero2';
 import Inquiry from './Inquiry';
 import Services from './Services'
@@ -19,7 +19,7 @@ const Hero = () => {
     <>  
      <motion.div {...fadeIn}
      
-      className="relative w-auto md:w-full h-[100px] md:h-[300px] lg:h-[400px] ">
+      className="relative w-auto md:w-full h-[100px] md:h-[500px] lg:h-[500px] ">
       <Carousel
         autoPlay
         infiniteLoop
@@ -30,19 +30,19 @@ const Hero = () => {
         className="h-full"
       >
         <div className="relative">
-          <img src={h1} alt="Slide 1" className="object-contain object-top w-full h-[300px] md:h-[300px] lg:h-[400px]" />
+          <img src={h1} alt="Slide 1" className="object-cover object-center w-full h-[300px] md:h-[500px] lg:h-[500px]" />
           
         </div> 
         <div className="relative">
-          <img src={h2} alt="Slide 2" className="object-contain object-top w-full h-[300px] md:h-[300px] lg:h-[400px]" />
+          <img src={h2} alt="Slide 2" className="object-cover object-center w-full h-[300px] md:h-[500px] lg:h-[500px]" />
           
         </div>
         <div className="relative">
-          <img src={h3} alt="Slide 3" className="object-contain object-top w-full h-[300px] md:h-[300px] lg:h-[400px]" />
+          <img src={h3} alt="Slide 3" className="object-cover object-center w-full h-[300px] md:h-[500px] lg:h-[500px]" />
           
         </div>
         {/* <div className="relative">
-          <img src={h5} alt="Slide 5" className="object-contain object-center w-full h-[300px] md:h-[300px] lg:h-[400px]" />
+          <img src={h5} alt="Slide 5" className="object-cover object-center w-full h-[300px] md:h-[500px] lg:h-[500px]" />
           
         </div> */}
       </Carousel>
